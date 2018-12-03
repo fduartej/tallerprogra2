04b3ba1e2505dcdaaa10627d6dea3c284eb222b9
user <user@user-PC>
2018-12-02 21:46:58 -0500

    Calculo de Salario y Funcion

--- src/tallerprogra/fundamental/Docente.java
+++ src/tallerprogra/fundamental/Docente.java
@@ -4,17 +4,41 @@ import java.util.Date;
 
 public class Docente {
 
+	private static final int MESES_DE_AGUINALDO_2 = 2;
+
+	private static final int MESES_DEL_AÑO_12 = 12;
+	
 	private int id;
 	private String firstName;
 	private String lastName;
 	private Date birthDay;
+	private Double salary;
 	
-	public Docente(int id, String firstName,String lastName,Date birthDay){
+	public Docente(int id, String firstName,String lastName,
+			Date birthDay,Double salary){
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.birthDay = birthDay;
+		this.salary = salary;
 	}
+	
+	public void calculateAnualSalary(){
+		double totalSalary = salary * (MESES_DEL_AÑO_12 + MESES_DE_AGUINALDO_2);
+		System.out.printf("El Salario Total %f \n", totalSalary);
+	}
+	
+	public Double getSalary() {
+		return salary;
+	}
+
+
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+
 
 	public int getId() {
 		return id;
--- src/tallerprogra/fundamental/DocenteApp.java
+++ src/tallerprogra/fundamental/DocenteApp.java
@@ -11,19 +11,20 @@ public class DocenteApp {
 		List<Docente> docentes = new ArrayList<Docente>();
 		Docente obj = null;
 		
-		obj = new Docente(1,"Jose","Perez",new Date());
+		obj = new Docente(1,"Jose","Perez",new Date(),1000.05);
 		docentes.add(obj);
 
 		
-		obj = new Docente(2,"Jose2","Perez2",new Date());
+		obj = new Docente(2,"Jose2","Perez2",new Date(),1500.10);
 		docentes.add(obj);
 		
-		obj = new Docente(3,"Ana","Perez",new Date());
+		obj = new Docente(3,"Ana","Perez",new Date(),1300.15);
 		docentes.add(obj);
 		
 		for(Docente o:docentes){
 			System.out.printf("ID: %d, Nombre %s, Apellido %s \n",
 					o.getId(),o.getFirstName(),o.getLastName());
+			o.calculateAnualSalary();
 		}
 	}
 
